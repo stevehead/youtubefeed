f499bc4588553a5a5a6633fbe030beea408263a8
Steve Johnson <sjohnson0684@gmail.com>
2015-04-20 13:34:43 -0400

    Added local settings import to main settings file.

--- youtubefeed/settings.py
+++ youtubefeed/settings.py
@@ -100,3 +100,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Local settings import
+try:
+    from local_settings import *
+except ImportError:
+    pass
