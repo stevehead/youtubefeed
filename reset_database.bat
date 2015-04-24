2c58636c809b1bce58721b855d1469e0adbadc1e
Steve Johnson <sjohnson0684@gmail.com>
2015-04-24 16:50:40 -0400

    Updated reset database batch file.

--- reset_database.bat
+++ reset_database.bat
@@ -2,6 +2,8 @@
 
 del "db.sqlite3"
 del "youtube\migrations\0001_initial.py"
+del "feed\migrations\0001_initial.py"
 python manage.py makemigrations youtube
+python manage.py makemigrations feed
 python manage.py migrate
 python manage.py createsuperuser
