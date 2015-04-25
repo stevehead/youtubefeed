a3f2123f7c83ffea809da502a93d8c4cd67ad22c
Steve Johnson <sjohnson0684@gmail.com>
2015-04-25 02:13:19 -0400

    BaseModel now has a cache dictionary that autoclears on save.

--- common/models.py
+++ common/models.py
@@ -6,5 +6,13 @@ class BaseModel(models.Model):
     record_created_at = models.DateTimeField('record creation time', auto_now_add=True)
     record_updated_at = models.DateTimeField('record update time', auto_now=True)
 
+    def __init__(self, *args, **kwargs):
+        super(BaseModel, self).__init__(*args, **kwargs)
+        self._cache_info = dict()
+
+    def save(self, *args, **kwargs):
+        super(BaseModel, self).save(*args, **kwargs)
+        self._cache_info = dict()
+
     class Meta:
         abstract = True
