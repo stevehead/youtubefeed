2486505711d847177e432d84328b1177c5346a89
Steve Johnson <sjohnson0684@gmail.com>
2015-04-26 00:55:38 -0400

    Home page added to URL patterns.

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from feed.urls import views
+
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'^feed/', include('feed.urls', namespace='feed')),
     url(r'^admin/', include(admin.site.urls)),
 ]
