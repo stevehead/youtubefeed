b942db769343b0444861d06add2a79df2af50c42
Steve Johnson <sjohnson0684@gmail.com>
2015-04-25 23:32:22 -0400

    Feed admin for better dev.
    Will not use the admin page in the end.

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Show, ShowType, ShowViewing, FeedChannel, FeedVideo
+
+
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ('name', 'channel_titles', 'status', )
+    ordering = ('record_created_at', 'name')
+
+
+admin.site.register(Show, ShowAdmin)
+admin.site.register(ShowType)
+admin.site.register(ShowViewing)
+admin.site.register(FeedChannel)
+admin.site.register(FeedVideo)
