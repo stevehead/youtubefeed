c8d3ee466f02b9985839f0d52abaf3fa0f11a07a
Steve Johnson <sjohnson0684@gmail.com>
2015-04-26 20:40:49 -0400

    API calls that fail throw a model-specific error.

--- youtube/models.py
+++ youtube/models.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from common.models import BaseModel
 from common.utils import convert_iso8601_duration_to_seconds, convert_iso8601_to_datetime
-from .utils import YoutubeAPIQuery
+from .utils import YoutubeAPIQuery, YoutubeAPIError
 
 
 class YoutubeModel(BaseModel):
@@ -21,6 +21,9 @@ class YoutubeModel(BaseModel):
     class Meta:
         abstract = True
 
+    class YoutubeQueryError(Exception):
+        pass
+
 
 class VideoModel(YoutubeModel):
     id = models.CharField(primary_key=True, max_length=11)
@@ -31,14 +34,20 @@ class VideoModel(YoutubeModel):
 
     @classmethod
     def create_from_youtube(cls, video_id):
-        video_info = YoutubeAPIQuery.get_video(video_id)
+        try:
+            video_info = YoutubeAPIQuery.get_video(video_id)
+        except YoutubeAPIError:
+            raise cls.YoutubeQueryError("Video could not be created from Youtube.")
         video = cls()
         video.set_info_from_youtube(video_info)
         video.save()
         return video
 
     def update_from_youtube(self):
-        video_info = YoutubeAPIQuery.get_video(self.pk)
+        try:
+            video_info = YoutubeAPIQuery.get_video(self.pk)
+        except YoutubeAPIError:
+            raise self.YoutubeQueryError("Video could not be updated from Youtube.")
         self.set_info_from_youtube(video_info)
         self.save()
 
@@ -58,22 +67,31 @@ class Channel(YoutubeModel):
     def create_from_youtube(cls, channel_id=None, username=None):
         if (channel_id is None and username is None) or (channel_id is not None and username is not None):
             raise AttributeError("Exactly one attribute is required.")
-        if username is not None:
-            channel_info = YoutubeAPIQuery.get_user(username)
-        else:
-            channel_info = YoutubeAPIQuery.get_channel(channel_id)
+        try:
+            if username is not None:
+                channel_info = YoutubeAPIQuery.get_user(username)
+            else:
+                channel_info = YoutubeAPIQuery.get_channel(channel_id)
+        except YoutubeAPIError:
+            raise cls.YoutubeQueryError("Channel could not be created from Youtube.")
         channel = cls()
         channel.set_info_from_youtube(channel_info)
         channel.save()
         return channel
 
     def update_from_youtube(self):
-        channel_info = YoutubeAPIQuery.get_channel(self.pk)
+        try:
+            channel_info = YoutubeAPIQuery.get_channel(self.pk)
+        except YoutubeAPIError:
+            raise self.YoutubeQueryError("Channel could not be updated from Youtube.")
         self.set_info_from_youtube(channel_info)
         self.save()
 
     def get_latest_videos(self, *args, **kwargs):
-        return YoutubeAPIQuery.get_playlist_videos(self.uploads_playlist_id, *args, **kwargs)
+        try:
+            return YoutubeAPIQuery.get_playlist_videos(self.uploads_playlist_id, *args, **kwargs)
+        except:
+            raise self.YoutubeQueryError("Latest channel videos could be queried from Youtube.")
 
 
 class Video(VideoModel):
@@ -84,5 +102,8 @@ class Video(VideoModel):
         try:
             Channel.objects.get(pk=self.channel_id)
         except ObjectDoesNotExist:
-            Channel.create_from_youtube(channel_id=self.channel_id)
+            try:
+                Channel.create_from_youtube(channel_id=self.channel_id)
+            except YoutubeAPIError:
+                raise self.YoutubeQueryError("The channel of the video could be created from Youtube.")
         super(Video, self).save(*args, **kwargs)
